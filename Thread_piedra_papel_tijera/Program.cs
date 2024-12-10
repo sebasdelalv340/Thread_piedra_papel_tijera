2665fdf271b9439b7c338c5a034e7a3d5ea4ae03
sebasdelalv340 <148891336+sebasdelalv340@users.noreply.github.com>
2024-12-10 21:13:03 +0100

    Initial commit

---
+++ Thread_piedra_papel_tijera/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public delegate void ThreadStart();
+
+class ThreadTest
+{
+    static void Main()
+    {
+        Thread t = new Thread (Go);
+        t.Start(); // Run Go() on the new thread.
+        Go(); // Simultaneously run Go() in the main thread.
+    }
+    static void Go()
+    {
+        Console.WriteLine ("hello!");
+    }
+}
